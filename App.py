6f2f81dd9cfe3bd5fcb6d2894e64f7042eab312b
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-08 02:35:32 +0100

    initialize flask app

---
+++ App.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+from flask import Flask,redirect,url_for
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello !! this is the main page <h1> HELLO world </h1>"
+
+@app.route("/<name>")
+def user(name):
+    return "Hello {} ! ".format(name)
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("user",name="Admin !"))
+if __name__=="__main__":
+    app.run()
