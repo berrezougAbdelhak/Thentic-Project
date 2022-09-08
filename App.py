1ef426895ad0cc361cd842e3358a605e2005e9ce
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-08 03:08:50 +0100

    Testing condition with HTML

--- App.py
+++ App.py
@@ -1,17 +1,9 @@
-from unicodedata import name
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,render_template
 app=Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello !! this is the main page <h1> HELLO world </h1>"
-
 @app.route("/<name>")
-def user(name):
-    return "Hello {} ! ".format(name)
+def home(name):
+    return render_template("index.html",content=["abd","zng","mjd"])
 
-@app.route('/admin')
-def admin():
-    return redirect(url_for("user",name="Admin !"))
 if __name__=="__main__":
     app.run()
