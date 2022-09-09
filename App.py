473f205a9639204b82db0768da12face75d944b2
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-09 03:06:21 +0100

    Show NFT contract

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ app=Flask(__name__)
 #     return render_template("index.html")
 
 @app.route("/",methods=["POST","GET"])
-def login():
+def create_nft():
     if request.method=="POST" :
         contract_name=request.form["Cname"]
         contract_symbole=request.form["Csymbl"]
@@ -28,6 +28,19 @@ def login():
         print(contract_name)
         print(contract_symbole)
     return render_template("index.html")
+@app.route("/show_nft",methods=["POST","GET"])
+def show_nft():
+    if request.method=="POST" :
+        api=request.form["api"]
+        evm_id=request.form["id"]
+        url = "https://thentic.tech/api/contracts?key={}&chain_id={}".format(api,evm_id)
+       
+        print(api)
+
+        r = requests.get(url)
+        return render_template("show_nft.html",content=r.json()["contracts"])
+        # print(r.json()["contracts"])
+    return render_template("show_nft.html")
 
 if __name__=="__main__":
     app.run()
